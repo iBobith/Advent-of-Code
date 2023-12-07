9d51b80b39229faf7a4f49bd4548c25b46904301
Bobith <jansons.kris@gmail.com>
2023-12-07 15:04:34 +0200

    Day one part two

--- dayOne/script.js
+++ dayOne/script.js
@@ -1,30 +1,102 @@
+const fs = require("fs");
+const path = require("path");
+
+function wordToNumber(word) {
+  return {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "zero": 0,
+  }[word] ?? Number(word);
+}
+
 function calculateSum() {
-  const inputText = document.getElementById('inputText').value;
+
+  // Get the input text from the text box
+  // const inputText = document.getElementById('inputText').value;
+  const inputText = fs.readFileSync(
+    path.join(__dirname, "input")
+  ).toString();
+
+
+  // Split the input text into an array of lines
   const lines = inputText.split('\n');
+
+  // Initialize a variable to store the sum
   let sum = 0;
-  let logContent = '';
 
+  // Read through each line
   lines.forEach((line) => {
-    const numbers = line.replace(/\D/g, '');
-    let firstDigit, lastDigit;
+    const validStrings = [
+      "one", "1",
+      "two", "2",
+      "three", "3",
+      "four", "4",
+      "five", "5",
+      "six", "6",
+      "seven", "7",
+      "eight", "8",
+      "nine", "9",
+      "zero", "0",
+    ];
 
-    if (numbers) {
-      firstDigit = numbers.charAt(0);
-      lastDigit = numbers.charAt(numbers.length - 1);
+    let firstIndex = Infinity;
+    let firstValue = null;
+    let lastIndex = -Infinity
+    let lastValue = null;
 
-      const trueNumber = firstDigit + (numbers.length > 1 ? lastDigit : '');
-      let twoDigitNumber = parseInt(trueNumber, 10);
-
-      if (twoDigitNumber < 10) {
-        twoDigitNumber *= 11;
+    for (const digit of validStrings) {
+      const fresult = line.indexOf(digit);
+      const lresult = line.lastIndexOf(digit);
+      if (fresult < firstIndex) {
+        if (fresult === -1) continue;
+        firstIndex = fresult;
+        firstValue = digit;
       }
-
-      sum += twoDigitNumber;
-
-      logContent += `Original Line: ${line} => Numbers: ${numbers} => Two-Digit Number: ${twoDigitNumber}\n`;
+      if (lresult > lastIndex) {
+        if (lresult === -1) continue;
+        lastIndex = lresult;
+        lastValue = digit;
+      }
     }
+
+    console.debug("Line: " + line);
+    if (firstIndex === lastIndex) {
+      // console.debug("SAME INDEEEEXXXX")
+    }
+
+
+    if (firstValue === null && lastValue === null) {
+      console.debug(" => no digits");
+      return;
+    }
+    
+    
+    firstValue = wordToNumber(firstValue);
+    lastValue = wordToNumber(lastValue);
+    
+    console.debug(` => First and last: ${firstValue} and ${lastValue}`);
+    
+    const numberItself = firstValue*10 + lastValue;
+
+    console.debug(`Twodigitgfsdf: ${numberItself}`);
+
+    sum += numberItself;
   });
 
-  document.getElementById('result').innerText = 'Final sum: ' + sum;
-  document.getElementById('logContent').innerText = logContent;
+  console.log("Final sum: " + sum);
+
+  // // Display the result
+  // document.getElementById('result').innerText = 'Final sum: ' + sum;
+  
+  // // Update the log on the webpage
+  // document.getElementById('logContent').innerText = logContent;
 }
+
+calculateSum();
